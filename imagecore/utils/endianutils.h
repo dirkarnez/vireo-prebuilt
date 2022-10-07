65b06d891e916ad68a319283c5f6d09fd231cefc
dirkarnez <smalldirkalex@gmail.com>
2022-10-08 02:13:29 +0800

    - fix endianutils.h for Windows

--- imagecore/utils/endianutils.h
+++ imagecore/utils/endianutils.h
@@ -33,11 +33,11 @@
 
 #ifdef __APPLE__
 #include <libkern/OSByteOrder.h>
-#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htobe16(x) OSSwapHostToBigInt16(x) //
 #define htole16(x) OSSwapHostToLittleInt16(x)
 #define betoh16(x) OSSwapBigToHostInt16(x)
-#define letoh16(x) OSSwapLittleToHostInt16(x)
-#define htobe32(x) OSSwapHostToBigInt32(x)
+#define letoh16(x) OSSwapLittleToHostInt16(x) //
+#define htobe32(x) OSSwapHostToBigInt32(x) //
 #define htole32(x) OSSwapHostToLittleInt32(x)
 #define betoh32(x) OSSwapBigToHostInt32(x)
 #define letoh32(x) OSSwapLittleToHostInt32(x)
@@ -45,92 +45,98 @@
 #define htole64(x) OSSwapHostToLittleInt64(x)
 #define betoh64(x) OSSwapBigToHostInt64(x)
 #define letoh64(x) OSSwapLittleToHostInt64(x)
-#else
+#elif defined(__USE_BSD) || defined(__USE_GNU)
 #include <endian.h>
-#if defined(__USE_BSD) || defined(__USE_GNU)
 /* Conversion interfaces.  */
-#include <byteswap.h>
+    #include <byteswap.h>
 
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-#ifndef htobe16
-#define htobe16(x) __bswap_16(x)
-#endif
-#ifndef htole16
-#define htole16(x) (x)
-#endif
-#ifndef betoh16
-#define betoh16(x) __bswap_16(x)
-#endif
-#ifndef letoh16
-#define letoh16(x) (x)
-#endif
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
+    #ifndef htobe16
+    #define htobe16(x) __bswap_16(x)
+    #endif
+    #ifndef htole16
+    #define htole16(x) (x)
+    #endif
+    #ifndef betoh16
+    #define betoh16(x) __bswap_16(x)
+    #endif
+    #ifndef letoh16
+    #define letoh16(x) (x)
+    #endif
 
-#ifndef htobe32
-#define htobe32(x) __bswap_32(x)
-#endif
-#ifndef htole32
-#define htole32(x) (x)
-#endif
-#ifndef betoh32
-#define betoh32(x) __bswap_32(x)
-#endif
-#ifndef letoh32
-#define letoh32(x) (x)
-#endif
+    #ifndef htobe32
+    #define htobe32(x) __bswap_32(x)
+    #endif
+    #ifndef htole32
+    #define htole32(x) (x)
+    #endif
+    #ifndef betoh32
+    #define betoh32(x) __bswap_32(x)
+    #endif
+    #ifndef letoh32
+    #define letoh32(x) (x)
+    #endif
 
-#ifndef htobe64
-#define htobe64(x) __bswap_64(x)
-#endif
-#ifndef htole64
-#define htole64(x) (x)
-#endif
-#ifndef betoh64
-#define betoh64(x) __bswap_64(x)
-#endif
-#ifndef letoh64
-#define letoh64(x) (x)
-#endif
+    #ifndef htobe64
+    #define htobe64(x) __bswap_64(x)
+    #endif
+    #ifndef htole64
+    #define htole64(x) (x)
+    #endif
+    #ifndef betoh64
+    #define betoh64(x) __bswap_64(x)
+    #endif
+    #ifndef letoh64
+    #define letoh64(x) (x)
+    #endif
 
-#else /* __BYTE_ORDER == __LITTLE_ENDIAN */
-#ifndef htobe16
-#define htobe16(x) (x)
-#endif
-#ifndef htole16
-#define htole16(x) __bswap_16(x)
-#endif
-#ifndef be16toh
-#define be16toh(x) (x)
-#endif
-#ifndef le16toh
-#define le16toh(x) __bswap_16(x)
-#endif
+    #else /* __BYTE_ORDER == __LITTLE_ENDIAN */
+    #ifndef htobe16
+    #define htobe16(x) (x)
+    #endif
+    #ifndef htole16
+    #define htole16(x) __bswap_16(x)
+    #endif
+    #ifndef be16toh
+    #define be16toh(x) (x)
+    #endif
+    #ifndef le16toh
+    #define le16toh(x) __bswap_16(x)
+    #endif
 
-#ifndef htobe32
-#define htobe32(x) (x)
-#endif
-#ifndef htole32
-#define htole32(x) __bswap_32(x)
-#endif
-#ifndef betoh32
-#define betoh32(x) (x)
-#endif
-#ifndef letoh32
-#define letoh32(x) __bswap_32(x)
-#endif
+    #ifndef htobe32
+    #define htobe32(x) (x)
+    #endif
+    #ifndef htole32
+    #define htole32(x) __bswap_32(x)
+    #endif
+    #ifndef betoh32
+    #define betoh32(x) (x)
+    #endif
+    #ifndef letoh32
+    #define letoh32(x) __bswap_32(x)
+    #endif
 
-#ifndef htobe64
-#define htobe64(x) (x)
-#endif
-#ifndef htole64
-#define htole64(x) __bswap_64(x)
-#endif
-#ifndef betoh64
-#define betoh64(x) (x)
-#endif
-#ifndef letoh64
-#define letoh64(x) __bswap_64(x)
-#endif
+    #ifndef htobe64
+    #define htobe64(x) (x)
+    #endif
+    #ifndef htole64
+    #define htole64(x) __bswap_64(x)
+    #endif
+    #ifndef betoh64
+    #define betoh64(x) (x)
+    #endif
+    #ifndef letoh64
+    #define letoh64(x) __bswap_64(x)
+    #endif
 
-#endif /* __BYTE_ORDER == __LITTLE_ENDIAN */
-#endif /* __USE_BSD || __USE_GNU */
+    #endif /* __BYTE_ORDER == __LITTLE_ENDIAN */
+#else /* __USE_BSD || __USE_GNU */
+#include <stdlib.h>
+#include <winsock2.h>
+
+#define htobe16(v) htons(v)
+#define letoh16(v) (v)
+#define htobe32(v) htonl(v)
+
 #endif /* __APPLE__ */
